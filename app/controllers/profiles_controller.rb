7d2ea505207f3c8f472fabceb1d9c429cc5a12c8
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-08-15 17:04:07 +0900

    no message

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,17 +14,16 @@ class ProfilesController < ApplicationController
     #   @profile.assign_attributes(profile_params)
     #   @profile.save!
 
-    #   render json: @profile
-    # end
+    #  render json: @profile
 
     def create
       @profile = current_user.build_profile(profile_params)
       @profile.save!
-      
+
       render json: @profile
     end
     
-
+    
     private
     def profile_params
       params.require(:profile).permit(:avatar)
