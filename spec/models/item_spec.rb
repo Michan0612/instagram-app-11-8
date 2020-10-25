82b6bfb538d5134d6a790174c8b062afc9751f73
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-10-25 10:39:33 +0900

    no message

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   let!(:user) { create(:user) }
+  include ActionDispatch::TestProcess
 
     context '内容が入力されている場合' do
      
 
       before do
         @item = build(:item, user: user)
-        @item.photos = fixture_file_upload('/images/avatar1.png', '/images/avatar2.png')
+        @item.photos = fixture_file_upload(Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'images', 'avatar1.png'), 'image/png'))
+        binding.pry
       end
 
       it 'is valid with an image' do
         expect(@item).to be_valid
       end
-
-
     end
 
   # context '内容の文字が一文字の場合' do
