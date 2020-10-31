1b3cc86ae809ee51f19af99cf15df7379eacc8ab
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-10-31 11:12:14 +0900

    no message

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
     factory :item do
         content { Faker::Lorem.characters(number: 140) }
+        after(:build) do |item|
+
+            item.photos.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'images', 'avatar1.png')), filename: 'avatar1.png', content_type: 'image/png')
+        end
     end
 end
