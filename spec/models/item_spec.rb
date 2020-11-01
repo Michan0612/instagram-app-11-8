7f9cbc6633f208e84e000e73c3cd3c9405a7690a
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-11-01 13:17:34 +0900

    no message

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   let!(:user) { create(:user) }
-  let!(:item) { create(:item, user: user).photos }
+  let!(:item) { create(:item, user: user)}
   context '内容が入力されている場合' do
 
     it 'photosにレコードが存在する' do
