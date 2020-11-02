4fab210721384034db053d6f1120a693d0c81681
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-11-03 08:39:56 +0900

    no message

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "Items", type: :request do
   describe "GET /items" do
     it "works! (now write some real specs)" do
       get item_path(item.id)
+      binding.pry
       expect(response).to have_http_status(200)
     end
   end
