0028d943cc549d5df0d9d96831ba72cc9ff72efb
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-11-08 21:36:50 +0900

    no message

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     def show
         @item = Item.find_by(id: params[:id])
     end
