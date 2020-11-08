abd9875926b8c610f5460e558240199448f68971
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-11-08 18:18:19 +0900

    no message

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,12 @@
-# class UserSerializer < ActiveModel::Serializer
-#     include Rails.application.routes.url_helpers
+class UserSerializer < ActiveModel::Serializer
+    include Rails.application.routes.url_helpers
 
-#     attributes :id, :display_name, :avatar_with_image
-#     has_many :comments
-#     has_one :profile
+    attributes :id, :display_name, :avatar_with_image
+    has_many :comments
+    has_one :profile
 
-#     def avatar_with_image
-#         rails_blob_path(object.avatar_image) if object.avatar_image.attachment
-#     end
-# end
-#必要な記述なのでテストが終わったらコメントアウトを消しましょう
+    def avatar_with_image
+        rails_blob_path(object.avatar_image) if object.avatar_image.attachment
+    end
+end
+# 必要な記述なのでテストが終わったらコメントアウトを消しましょう
