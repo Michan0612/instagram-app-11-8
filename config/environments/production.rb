7bcc20e0f8a300bdb9089971b361a77ad2a2b64a
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-11-12 07:48:21 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "instagram_app_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'example.com'}
+  Rails.application.routes.default_url_options = { host: "your heroku domain" }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
